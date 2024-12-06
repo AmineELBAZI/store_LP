9c167354a0d0a3d4b8b1c83590d7394922776572
bibzaznenorsys <bibzazne@norsys.fr>
2024-12-06 20:41:57 +0100

    chore: add folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 
 function App() {
+  let name = 'badr'
   return (
-      <>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
-      </>
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h2></h2>
+    </>
   )
 }
 
